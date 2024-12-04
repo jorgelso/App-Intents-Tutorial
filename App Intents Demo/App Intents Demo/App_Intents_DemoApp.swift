f96ecd719ccffa0a1e71d7286b731ed56d667edf
Jorge Salcedo <165866373+jorgelso@users.noreply.github.com>
2024-12-04 15:16:45 -0600

    Initial Commit

---
+++ App Intents Demo/App Intents Demo/App_Intents_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_Intents_DemoApp.swift
+//  App Intents Demo
+//
+//  Created by Jorge Salcedo on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct App_Intents_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
